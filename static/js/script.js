0f470be5f0565476c704cf42b859ea799e700147
Edvin Larsson <edvinlarsson0110@gmail.com>
2024-01-22 15:51:38 +0000

    tabs on the accountoverview now working correctly

--- static/js/script.js
+++ static/js/script.js
@@ -7,25 +7,35 @@ django.jQuery(document).ready(function ($) {
 
 /* Script for URL Hashing in the accountoverview */
 django.jQuery(document).ready(function ($) {
-    // Function to update the URL hash when a tab is clicked
-    $('.nav-link').on('click', function () {
+  // Function to update the URL hash when a tab is clicked
+  $('.nav-link').on('click', function () {
       var tabId = $(this).attr('data-bs-target');
       window.location.hash = tabId.replace('#nav-', '#');
-    });
-  
-    // Function to activate the tab based on the URL hash
-    function activateTabFromHash() {
+  });
+
+  // Function to activate the tab based on the URL hash
+  function activateTabFromHash() {
       var hash = window.location.hash;
       if (hash) {
-        $('.nav-link').removeClass('active');
-        $('button[data-bs-target="' + hash + '"]').tab('show').addClass('active');
+          // Use Bootstrap's Tab JavaScript methods to activate the tab
+          var tabSelector = 'button[data-bs-target="' + hash + '"]';
+          var tab = $(tabSelector)[0];
+          if (tab) {
+              var tabPaneId = $(tab).attr('data-bs-target');
+              var tabContentId = $(tabPaneId).attr('id');
+              var tabInstance = new bootstrap.Tab(tab);
+              tabInstance.show();
+              $('#' + tabContentId).addClass('show active');
+          }
       }
-    }
-  
-    // Listen for changes in the URL hash
-    $(window).on('hashchange', activateTabFromHash);
-  
-    // Activate the initial tab based on the URL hash
-    activateTabFromHash();
-  });
+  }
+
+  // Listen for changes in the URL hash
+  $(window).on('hashchange', activateTabFromHash);
+
+  // Activate the initial tab based on the URL hash
+  activateTabFromHash();
+});
+
+
   
